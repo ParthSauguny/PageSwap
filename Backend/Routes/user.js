af9a138520f4ca58dba3cbb2ba4ab2a9e6976cd2
ParthSauguny <parthsauguny@gmail.com>
2024-07-06 23:52:03 +0530

    added database models

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const usermodel = require("../models/user");
 
 router.get("/signup" , (req,res) => {
     res.render("signup");
+});
+router.post("/signup",(req,res) => {
+    usermodel.create({
+        name: username,
+        email:email,
+        password:password,
+    })
+    res.redirect("/");
 });
 
 router.get("/login" , (req,res) => {
---
+++ Backend/models/books.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+const bookSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    genre:{
+        type:String,
+        required:true,
+    },
+    wannaDonate:{
+        required:true,
+        type:Boolean,
+        default:false,
+    },
+    price:{
+        type:Number,
+        required:false,
+    },
+});
