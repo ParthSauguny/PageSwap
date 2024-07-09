7307421865aae90e55b78ef436185b8d32b0ab24
ParthSauguny <parthsauguny@gmail.com>
2024-07-09 21:23:48 +0530

    adding pages

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -7,11 +7,6 @@ function Login() {
       <h1 className='text-center text-6xl my-2'>Login</h1>
       <form action="/" method="post" className=' flex justify-between flex-col px-24 my-6'>
           <div className=' flex justify-between'>
-              <label>Enter your name</label>
-              <input type="text" placeholder='username' />
-          </div>
-
-          <div className=' flex justify-between'>
               <label>Enter your email</label>
               <input type="email" placeholder='email' />
           </div>
@@ -19,6 +14,10 @@ function Login() {
           <div className=' flex justify-between'>
               <label>Enter your password</label>
               <input type="password" placeholder='password' />
+          </div>
+
+          <div>
+            <button className=' bg-green-600 border-orange-500 border-2 rounded-lg p-2'>Login</button>
           </div>
 
       </form>
