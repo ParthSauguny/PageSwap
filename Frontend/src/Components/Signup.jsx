e9d7fc8e6312ee164e7f2b04a496ce881e774751
ParthSauguny <parthsauguny@gmail.com>
2024-09-20 02:51:28 +0530

    commit

--- Frontend/src/Components/Signup.jsx
+++ Frontend/src/Components/Signup.jsx
@@ -1,28 +1,54 @@
 import React from 'react'
+import axios from 'axios'
 import {Link} from "react-router-dom"
+import { useState } from 'react';
 
 function Signup() {
+  const [signupData , setsignupData] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+
+  const changeHandler = (event)=> {
+    setsignupData((prevData) => {
+      return {
+        ...signupData,
+        [event.target.name] : event.target.value
+      }
+    })
+  }
+
+  function submitHandler(e){
+    e.preventDefault();
+    axios.post("http://localhost:8000/user/login" ,
+      {username: signupData.username,
+      email: signupData.email,
+      password: signupData.password,}
+    )
+  }
+
   return (
     <div className='flex justify-between flex-col mx-96 bg-slate-800'>
       <h1 className='text-center text-6xl my-2'>Sign up</h1>
       <form action="/" method="post" className=' flex justify-between flex-col px-24 my-6'>
           <div className=' flex justify-between m-4 text-lg'>
               <label>Enter your username</label>
-              <input type="text" placeholder='username' name='username' />
+              <input type="text" onChange={changeHandler} placeholder='username' name='username' />
           </div>
 
           <div className=' flex justify-between m-4 text-xl'>
               <label>Enter your email</label>
-              <input type="email" placeholder='email' name='email'/>
+              <input type="email" onChange={changeHandler} placeholder='email' name='email'/>
           </div>
 
           <div className=' flex justify-between m-4 text-lg'>
               <label>Enter your password</label>
-              <input type="password" placeholder='password' name='password'/>
+              <input type="password" onChange={changeHandler} placeholder='password' name='password'/>
           </div>
 
           <div className='mx-44 my-5'>
-            <button  className='bg-black hover:bg-slate-400 rounded-lg p-2'>Sign Up</button>
+            <button onClick={submitHandler} className='bg-black hover:bg-slate-400 rounded-lg p-2'>Sign Up</button>
           </div>
 
       </form>
