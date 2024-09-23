d6767eb82e6005a51e994426f992d13778587dcd
ParthSauguny <parthsauguny@gmail.com>
2024-09-24 01:41:51 +0530

    commit

--- Backend/Routes/booksroutes.js
+++ Backend/Routes/booksroutes.js
@@ -3,32 +3,31 @@ const router = express.Router();
 const {Book} = require("../models/books");
 
 router.post("/add-book" , async(req,res) => {
-    const {title , genre , address} = req.body;
-    await ExchangeBook.create({
-        title,
-        genre,
-        address,
-    })
-    res.redirect('/');
+    try {
+        await Book.create({
+            title: req.body.title,
+            owner: req.body.owner,
+            borrower: req.body.borrower || null, // Optional, default to null if not provided
+            exchangeRequest: {
+              requestedBy: req.body.exchangeRequest?.requestedBy || null, // Optional, default to null
+              requestedTo: req.body.exchangeRequest?.requestedTo || null,
+              exchangeBookId: req.body.exchangeRequest?.exchangeBookId || null,
+              status: req.body.exchangeRequest?.status || null
+            },
+            history: req.body.history || [], // Optional, default to an empty array
+            author: req.body.author,
+            genre: req.body.genre,
+            available: req.body.available !== undefined ? req.body.available : true, // Default to true if not provided
+            address: req.body.address,
+            price: req.body.price || 0 // Optional, default to 0 if not provided
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
-router.get("/exchange" , (req,res) => {
-    res.send("this is take page");
-});
-
-router.get("/borrow" , (req,res) => {
-    res.send("this is take page");
-});
-
-router.post("/borrow" , async(req,res) =>{
-    const {title , genre , price , pickupAddress} = req.body;
-    await BorrowBook.create({
-        title,
-        genre,
-        price,
-        pickupAddress,
-    });
-    res.redirect("/");
+router.get("/add-book" , (req,res) => {
+    res.send("this is book page");
 });
 
 module.exports = router;
