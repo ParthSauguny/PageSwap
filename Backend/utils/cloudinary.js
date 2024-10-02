1a776fd0cbb76405cde24264223907ee39d076c5
ParthSauguny <parthsauguny@gmail.com>
2024-10-03 01:15:10 +0530

    cloudinary setup

---
+++ Backend/utils/cloudinary.js
@@ -0,0 +1,26 @@
+const v2 = require('cloudinary');
+const fs = require('fs');
+require('dotenv').config();
+
+v2.config({
+    cloud_name: process.env.COUDINARY_CLOUD_NAME,
+    api_key: process.env.COUDINARY_API_KEY,
+    api_secret: process.env.COUDINARY_API_SECRET,
+});
+
+const uploadCloudinary = async (localfilepath) => {
+    if(!localfilepath) return null;
+    try {
+        const res = await v2.uploader.upload(localfilepath , {
+            resource_type: "auto"
+        })
+        //file uploaded yayyyy
+        console.log("uploaded yayyyy");
+        return res;
+    } catch (error) {
+        fs.unlinkSync(localfilepath); //since upload got failed, we delete the temp img file from server
+        return null;
+    }
+}
+
+export {uploadCloudinary}
